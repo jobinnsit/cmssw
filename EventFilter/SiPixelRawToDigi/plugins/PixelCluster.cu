13e2bbd18a520599ecddee467cbdf9845b924497
Sushil Dubey <sushildubey171@gmail.com>
2017-06-19 09:36:50 +0200

    remove unified memory from cluster due to performance issue

--- EventFilter/SiPixelRawToDigi/plugins/PixelCluster.cu
+++ EventFilter/SiPixelRawToDigi/plugins/PixelCluster.cu
@@ -506,6 +506,11 @@ __global__ void cluster_kernel(uint *xx, uint *yy, const int *mIndexStart,
 
 } //End of cluster_kernel
 
+// assign the index of last cluster
+__global__ void init_kernel(const uint wordCounter, const uint total_cluster, uint *Index) {
+  Index[total_cluster] = wordCounter;
+}
+
 /************* origin of call to the kernel***************/
 
 void PixelCluster_Wrapper(uint *d_xx, uint *d_yy, uint *d_ADC,const uint wordCounter,
@@ -557,38 +562,12 @@ void PixelCluster_Wrapper(uint *d_xx, uint *d_yy, uint *d_ADC,const uint wordCou
     cout<<"Total_clusters after sub-clustering: "<<total_cluster<<endl;
     cout<<"Clustering finished on GPU!"<<endl;
     // call CPE function
-    Index[total_cluster] = wordCounter;
+    init_kernel<<<1,1>>>(wordCounter, total_cluster, Index);
+    //Index[total_cluster] = wordCounter;
     //since origin is shifted by (1,1) move it back to (0,0) before giving it CPE
     shift_origin_kernel<<<N_blocks, N_threads>>>(wordCounter,d_xx,d_yy); 
     cudaDeviceSynchronize();
     CPE_wrapper(total_cluster,d_gClusterId1, Index, d_xx, d_yy, d_ADC);
-
-  /*  
-    int count = wordCounter*sizeof(uint);
-    cudaMemcpy(xx, d_xx,count , cudaMemcpyDeviceToHost);
-    cudaMemcpy(yy, d_yy,count , cudaMemcpyDeviceToHost);
-    cudaMemcpy(gClusterId, d_gClusterId1, count , cudaMemcpyDeviceToHost);
-    cudaMemcpy(adc_h, d_ADC, count, cudaMemcpyDeviceToHost);
-    cudaDeviceSynchronize();
-
-    ofstream cfile;
-    cfile.open("Input_for_CPE_GPU_PartA.txt");
-    cfile<<"Index\tClusterId"<<endl;
-    for (int i = 0; i < total_cluster; i++) {
-      //cfile<<setw(10)<<gClusterId[i]<<setw(6)<<xx[i]<<setw(6)<<yy[i]<<setw(10)<<adc_h[i]<<endl;
-      cfile<<setw(6)<<Index[i]<<setw(14)<<gClusterId[i]<<endl;
-    }
-    cfile.close();
-     cudaMemcpy(gClusterId, d_gClusterId, count , cudaMemcpyDeviceToHost);
-    cfile.open("Input_for_CPE_GPU_PartB.txt");
-    cfile<<"ClusterId\t\txx\tyy\tADC"<<endl;
-    for (int i = 0; i < wordCounter; i++) {
-      //cfile<<setw(10)<<gClusterId[i]<<setw(6)<<xx[i]<<setw(6)<<yy[i]<<setw(10)<<adc_h[i]<<endl;
-      cfile<<setw(6)<<i<<setw(14)<<gClusterId[i]<<setw(6)<<xx[i]<<setw(6)<<yy[i]<<setw(10)<<adc_h[i]<<endl;
-    }
-    cfile.close();
-  */  
-    // move it into destructor
     
 } //end of pixel clusterizer
 
@@ -596,8 +575,7 @@ void initDeviceMemCluster() {
     const int MAX_FED = 150; // not all are present typically 108
     const int MAX_WORD = 2000; // don't know the exact max word, for PU70 max was 2900
     const int size = MAX_FED*MAX_WORD*sizeof(uint);
-    cudaMallocManaged((void**)&Index , size*sizeof(uint));
-    checkCUDAError("Error in unified memory allocation");
+    cudaMalloc((void**)&Index , size*sizeof(uint));
     cudaMalloc((void**)&d_xx1, size*sizeof(uint));
     cudaMalloc((void**)&d_yy1, size*sizeof(uint));
     cudaMalloc((void**)&d_gClusterId, size*sizeof(uint));
