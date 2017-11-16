bdbd192565ec5a58b8bab308476565d5794442a1
Andrea Bocci <fwyzard@gmail.com>
2017-11-16 18:25:10 +0100

    rename CUDA plugins to avoid conflict with standard ones

--- EventFilter/SiPixelRawToDigi/plugins/SiPixelRawToDigiGPU.cc
+++ EventFilter/SiPixelRawToDigi/plugins/SiPixelRawToDigiGPU.cc
@@ -42,7 +42,7 @@
 
 #include "TH1D.h"
 #include "TFile.h"
-#include "SiPixelRawToDigi.h"
+#include "SiPixelRawToDigiGPU.h"
 #include <string>
 #include <chrono>
 #include <iostream>
@@ -61,7 +61,7 @@
 using namespace std;
 
 // -----------------------------------------------------------------------------
-SiPixelRawToDigi::SiPixelRawToDigi( const edm::ParameterSet& conf ) 
+SiPixelRawToDigiGPU::SiPixelRawToDigiGPU( const edm::ParameterSet& conf ) 
   : config_(conf), 
     badPixelInfo_(nullptr),
     regions_(nullptr),
@@ -111,14 +111,14 @@ SiPixelRawToDigi::SiPixelRawToDigi( const edm::ParameterSet& conf )
   usePilotBlade = false; 
   if (config_.exists("UsePilotBlade")) {
     usePilotBlade = config_.getParameter<bool> ("UsePilotBlade");
-    if(usePilotBlade) edm::LogInfo("SiPixelRawToDigi")  << " Use pilot blade data (FED 40)";
+    if(usePilotBlade) edm::LogInfo("SiPixelRawToDigiGPU")  << " Use pilot blade data (FED 40)";
   }
 
   // Control the usage of phase1
   usePhase1 = false;
   if (config_.exists("UsePhase1")) {
     usePhase1 = config_.getParameter<bool> ("UsePhase1");
-    if(usePhase1) edm::LogInfo("SiPixelRawToDigi")  << " Using phase1";
+    if(usePhase1) edm::LogInfo("SiPixelRawToDigiGPU")  << " Using phase1";
   }
   //CablingMap could have a label //Tav
   cablingMapLabel = config_.getParameter<std::string> ("CablingMapLabel");
@@ -157,8 +157,8 @@ SiPixelRawToDigi::SiPixelRawToDigi( const edm::ParameterSet& conf )
 
 
 // -----------------------------------------------------------------------------
-SiPixelRawToDigi::~SiPixelRawToDigi() {
-  edm::LogInfo("SiPixelRawToDigi")  << " HERE ** SiPixelRawToDigi destructor!";
+SiPixelRawToDigiGPU::~SiPixelRawToDigiGPU() {
+  edm::LogInfo("SiPixelRawToDigiGPU")  << " HERE ** SiPixelRawToDigiGPU destructor!";
 
   if (regions_) delete regions_;
 
@@ -186,7 +186,7 @@ SiPixelRawToDigi::~SiPixelRawToDigi() {
 }
 
 void
-SiPixelRawToDigi::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+SiPixelRawToDigiGPU::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.add<bool>("IncludeErrors",true);
   desc.add<bool>("UseQualityInfo",false);
@@ -217,14 +217,14 @@ SiPixelRawToDigi::fillDescriptions(edm::ConfigurationDescriptions& descriptions)
   desc.add<std::string>("CablingMapLabel","")->setComment("CablingMap label"); //Tav
   desc.addOptional<bool>("CheckPixelOrder");  // never used, kept for back-compatibility
   desc.add<bool>("ConvertADCtoElectrons", false)->setComment("## do the calibration ADC-> Electron and apply the threshold, requried for clustering");
-  descriptions.add("siPixelRawToDigi",desc);
+  descriptions.add("siPixelRawToDigiGPU",desc);
 }
 
 // -----------------------------------------------------------------------------
 
 
 // -----------------------------------------------------------------------------
-void SiPixelRawToDigi::produce( edm::Event& ev,
+void SiPixelRawToDigiGPU::produce( edm::Event& ev,
                               const edm::EventSetup& es) 
 {
   //const uint32_t dummydetid = 0xffffffff;
@@ -276,8 +276,8 @@ void SiPixelRawToDigi::produce( edm::Event& ev,
   if (regions_) {
     regions_->run(ev, es);
     formatter.setModulesToUnpack(regions_->modulesToUnpack());
-    LogDebug("SiPixelRawToDigi") << "region2unpack #feds: "<<regions_->nFEDs();
-    LogDebug("SiPixelRawToDigi") << "region2unpack #modules (BPIX,EPIX,total): "<<regions_->nBarrelModules()<<" "<<regions_->nForwardModules()<<" "<<regions_->nModules();
+    LogDebug("SiPixelRawToDigiGPU") << "region2unpack #feds: "<<regions_->nFEDs();
+    LogDebug("SiPixelRawToDigiGPU") << "region2unpack #modules (BPIX,EPIX,total): "<<regions_->nBarrelModules()<<" "<<regions_->nForwardModules()<<" "<<regions_->nModules();
   }*/
   // GPU specific: Data extraction for RawToDigi GPU
   static unsigned int wordCounterGPU =0;
@@ -314,80 +314,6 @@ void SiPixelRawToDigi::produce( edm::Event& ev,
       continue;
     }
 
-<<<<<<< HEAD
-    //get event data for this fed
-    const FEDRawData& fedRawData = buffers->FEDData( fedId );
-
-    //convert data to digi and strip off errors
-    formatter.interpretRawData( errorsInEvent, fedId, fedRawData, *collection, errors);
-
-    //pack errors into collection
-    if(includeErrors) {
-      typedef PixelDataFormatter::Errors::iterator IE;
-      for (IE is = errors.begin(); is != errors.end(); is++) {
-	uint32_t errordetid = is->first;
-	if (errordetid==dummydetid) {           // errors given dummy detId must be sorted by Fed
-	  nodeterrors.insert( nodeterrors.end(), errors[errordetid].begin(), errors[errordetid].end() );
-	} else {
-	  edm::DetSet<SiPixelRawDataError>& errorDetSet = errorcollection->find_or_insert(errordetid);
-	  errorDetSet.data.insert(errorDetSet.data.end(), is->second.begin(), is->second.end());
-	  // Fill detid of the detectors where there is error AND the error number is listed
-	  // in the configurable error list in the job option cfi.
-	  // Code needs to be here, because there can be a set of errors for each 
-	  // entry in the for loop over PixelDataFormatter::Errors
-
-	  std::vector<PixelFEDChannel> disabledChannelsDetSet;
-
-	  for (auto const& aPixelError : errorDetSet) {
-	    // For the time being, we extend the error handling functionality with ErrorType 25
-	    // In the future, we should sort out how the usage of tkerrorlist can be generalized
-	    if (aPixelError.getType()==25) {
-	      assert(aPixelError.getFedId()==fedId);
-	      const sipixelobjects::PixelFEDCabling* fed = cabling_->fed(fedId);
-	      if (fed) {
-		cms_uint32_t linkId = formatter.linkId(aPixelError.getWord32());
-		const sipixelobjects::PixelFEDLink* link = fed->link(linkId);
-		if (link) {
-		  // The "offline" 0..15 numbering is fixed by definition, also, the FrameConversion depends on it
-		  // in contrast, the ROC-in-channel numbering is determined by hardware --> better to use the "offline" scheme
-		  PixelFEDChannel ch = {fed->id(), linkId, 25, 0};
-		  for (unsigned int iRoc=1; iRoc<=link->numberOfROCs(); iRoc++) {
-		    const sipixelobjects::PixelROC * roc = link->roc(iRoc);
-		    if (roc->idInDetUnit()<ch.roc_first) ch.roc_first=roc->idInDetUnit();
-		    if (roc->idInDetUnit()>ch.roc_last) ch.roc_last=roc->idInDetUnit();
-		  }
-		  disabledChannelsDetSet.push_back(ch);
-		}
-	      }
-	    } else {
-	      // fill list of detIds to be turned off by tracking
-	      if(!tkerrorlist.empty()) {
-		std::vector<int>::iterator it_find = find(tkerrorlist.begin(), tkerrorlist.end(), aPixelError.getType());
-		if(it_find != tkerrorlist.end()){
-		  tkerror_detidcollection->push_back(errordetid);
-		}
-	      }
-	    }
-
-	    // fill list of detIds with errors to be studied
-	    if(!usererrorlist.empty()) {
-	      std::vector<int>::iterator it_find = find(usererrorlist.begin(), usererrorlist.end(), aPixelError.getType());
-	      if(it_find != usererrorlist.end()){
-		usererror_detidcollection->push_back(errordetid);
-	      }
-	    }
-
-	  } // loop on DetSet of errors
-
-	  if (!disabledChannelsDetSet.empty()) {
-	    disabled_channelcollection->insert(errordetid, disabledChannelsDetSet.data(), disabledChannelsDetSet.size());
-	  }
-
-	} // if error assigned to a real DetId
-      } // loop on errors in event for this FED
-    } // if errors to be included in the event
-  } // loop on FED data to be unpacked
-=======
     // check headers
     const Word64* header = reinterpret_cast<const Word64* >(rawData.data()); header--;
     bool moreHeaders = true;
@@ -397,7 +323,6 @@ void SiPixelRawToDigi::produce( edm::Event& ev,
       bool headerStatus = errorcheck.checkHeader(errorsInEvent, fedId, header, errors);
       moreHeaders = headerStatus;
     }
->>>>>>> checkout RawToDigi part from GPU_RawToCPE package and add memory transfer
 
     // check trailers
     bool moreTrailers = true;
@@ -454,17 +379,6 @@ void SiPixelRawToDigi::produce( edm::Event& ev,
 } //end of produce function
 
 //define as runnable module
-DEFINE_FWK_MODULE(SiPixelRawToDigi);
 
-<<<<<<< HEAD
-  //send digis and errors back to framework 
-  ev.put(std::move(collection));
-  if(includeErrors){
-    ev.put(std::move(errorcollection));
-    ev.put(std::move(tkerror_detidcollection));
-    ev.put(std::move(usererror_detidcollection), "UserErrorModules");
-    ev.put(std::move(disabled_channelcollection));
-  }
-}
-=======
->>>>>>> checkout RawToDigi part from GPU_RawToCPE package and add memory transfer
+DEFINE_FWK_MODULE(SiPixelRawToDigiGPU);
+
